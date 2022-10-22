54061fd2db3f5ca1b0eb26c26400aeb94d1ede3e
Caio Amaral Corrêa <caiobep@me.com>
2022-10-21 21:45:05 -0300

    chore: jest setup

--- jest.config.js
+++ jest.config.js
@@ -8,6 +8,10 @@ const customJestConfig = {
   testPathIgnorePatterns: [
     '/e2e/'
   ],
+  moduleNameMapper: {
+    '^@/components/(.*)$': '<rootDir>/components/$1',
+    '^@/pages/(.*)$': '<rootDir>/pages/$1',
+  },
   moduleDirectories: ['node_modules', '<rootDir>/'],
   testEnvironment: 'jest-environment-jsdom',
 }
