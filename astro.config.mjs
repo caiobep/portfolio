d0f36f587274ea85383da9420f93ba999bea41d4
Caio Amaral Corrêa <caiobep@me.com>
2023-01-09 20:43:51 -0300

    feat: move to astro

---
+++ astro.config.mjs
@@ -0,0 +1,9 @@
+import { defineConfig } from "astro/config";
+
+import tailwind from "@astrojs/tailwind";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+
+export default defineConfig({
+  integrations: [tailwind(), mdx(), sitemap()],
+});
